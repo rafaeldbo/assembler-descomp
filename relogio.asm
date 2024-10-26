5e778b20b0b9a1edac81fbce38a2dab5e64c2b65
rafaeldbo <rafaeldbo@al.insper.edu.br>
2024-10-26 14:50:09 -0300

    Relógio com aceleração e valor de início

--- relogio.asm
+++ relogio.asm
@@ -2,6 +2,7 @@
     #define LEDR0TO7 256
     #define LEDR8 257
     #define LEDR9 258
+    #define SEL_TIMER 259
     #define HEX0 288
     #define HEX1 289
     #define HEX2 290
@@ -9,11 +10,11 @@
     #define HEX4 292
     #define HEX5 293
     #define SW0TO7 320
+    #define TIMER 323
     #define KEY0 352
     #define KEY1 353
     #define FPGA_RESET 356
-    #define TIMER_SEG 357
-    #define TIMER_SEG_RESET 506
+    #define TIMER_RESET 506
     #define KEY1_RESET 510
     #define KEY0_RESET 511
 
@@ -32,6 +33,8 @@
     #define LIM_DEZ_MILHARES 26
     #define LIM_CEN_MILHARES 27
 
+    #define SPEED_STATE 48
+
     #define INC_DISABLE 32 # flag que desabilita o incremento
 
     # Zerando Hexas e Apagando Leds
@@ -45,10 +48,15 @@
         STA R0 @LEDR0TO7
         STA R0 @LEDR8
         STA R0 @LEDR9
+    
+    # Configurando timer para 1 segundo
+        STA R0 @SEL_TIMER
+        STA R0 @SPEED_STATE
 
     # Limpando FlipFlop dos botôes
         STA R0 @KEY0_RESET
         STA R0 @KEY1_RESET
+        STA R0 @TIMER_RESET
 
     # Inicializando Variáveis
         LDI R0 $0 # contagem inciada em 0
@@ -58,14 +66,6 @@
         STA R0 @UNI_MILHARES
         STA R0 @DEZ_MILHARES
         STA R0 @CEN_MILHARES
-
-        LDI R0 $9 # Limite de contagem inicial de 999.999
-        STA R0 @LIM_UNIDADES
-        STA R0 @LIM_DEZENAS
-        STA R0 @LIM_CENTENAS
-        STA R0 @LIM_UNI_MILHARES
-        STA R0 @LIM_DEZ_MILHARES
-        STA R0 @LIM_CEN_MILHARES
 
     # Variaveis utilizadas em operações
         LDI R0 $0
@@ -78,35 +78,47 @@
         STA R0 @4
         LDI R0 $6
         STA R0 @6
+        LDI R0 $7
+        STA R0 @7
         LDI R0 $10
         STA R0 @10 
+        LDI R0 $15
+        STA R0 @15
     # Fim do setup
 
 # loop principal
 loop:
-    # Verifica se pediu para reiniciar
+    # Verifica se pediu para resetar
     LDA R0 @FPGA_RESET # verifica FPGA_RESET
     AND R0 @1
-    CEQ R0 @1 # Não tem debouncing no FPGA_RESET, então o botão é 0 quando apertado
-    JEQ @notRestart # Só reinicia a contagem se o FPGA_RESET for pressionado
-    JSR @restart
-    notRestart:
+    CEQ R0 @1 # Não tem debouncing no FPGA_RESET, então o botão é 0 enquanto apertado
+    JEQ @notReset # Só reseta a contagem se o FPGA_RESET for pressionado
+    JSR @reset
+    notReset:
 
-    # Verifica se pediu para configurar limite
+    # Verifica se pediu para configurar valor de início
     LDA R0 @KEY1 # verifica KEY1
     AND R0 @1
     CEQ R0 @0
-    JEQ @notConfig # Só muda pro modo de configuração de limite se o KEY1 for pressionado
+    JEQ @notConfig # Só muda pro modo de configuração do valor de início se o KEY1 for pressionado
     JSR @config
     notConfig:
 
     # Verifica se pediu para incrementar
-    LDA R0 @TIMER_SEG # verifica KEY0
+    LDA R0 @TIMER # verifica timer
     AND R0 @1
     CEQ R0 @0
-    JEQ @notInc # Só incrementa de limite se o KEY0 for pressionado
+    JEQ @notInc # Só incrementa se o timer tiver "estourado"
     JSR @inc
     notInc:    
+
+    # Verifica se pediu para mudar a velocidade
+    LDA R0 @KEY0 # verifica KEY0
+    AND R0 @1
+    CEQ R0 @0
+    JEQ @notSpeed # Só muda a velocidade de incremento se o KEY0 for pressionado
+    JSR @speed
+    notSpeed: 
 
     JSR @updateHexas # exibe contagem atualizada
     JMP @loop # fim do loop principal
@@ -114,9 +126,19 @@ loop:
 
 # Sub-rotinas
 
+# Função para trocar velocidade do timer
+speed:
+    STA R0 @KEY0_RESET
+    LDA R0 @SPEED_STATE
+    ADD R0 @1 # "inverte" o seletor de velocidade do timer
+    STA R0 @SPEED_STATE
+    STA R0 @SEL_TIMER
+    STA R0 @LEDR9
+    RET
+
 # Função para incrementar
 inc:
-    STA R0 @TIMER_SEG_RESET # limpa botão de incrementar
+    STA R0 @TIMER_RESET # limpa botão de incrementar
     
     LDA R0 @INC_DISABLE
     CEQ R0 @1 # verifica flag de limite de contagem / overflow (inibe incremento)
@@ -127,49 +149,14 @@ inc:
     STA R0 @UNIDADES
 
     JSR @update # atualiza valores para ficarem decimais
-    JSR @checkLimit # checa se limite de contagem foi atingido
 
     endInc:
     RET
 
-# Função para configurar limites
+# Função para configurar valor inicial
 config:
-    STA R0 @KEY1_RESET # limpa botão de configurar limites
-    JSR @restart # reseta contagem
-
-    LDI R0 $1 # indica que está setando limite da unidade (1 led acesos)
-    STA R0 @LEDR0TO7
-
-    configUnidades:
-    LDA R0 @KEY1
-    AND R0 @1
-    CEQ R0 @0 # verifica se o botão de configurar foi apertado
-    LDA R0 @SW0TO7
-    STA R0 @HEX0 # exibe valor das chaves
-    JEQ @configUnidades
-    STA R0 @KEY1_RESET
-    STA R0 @LIM_UNIDADES
-    LDI R0 $9
-    CLT R0 @LIM_UNIDADES # verifica se o limite é válido
-    JLT @configUnidades
-    # Se o limite é valido, passa pro próximo
-
-    LDI R0 $3 # indica que está setando limite da dezena (2 leds acesos)
-    STA R0 @LEDR0TO7
-
-    configDezenas:
-    LDA R0 @KEY1
-    AND R0 @1
-    CEQ R0 @0 # verifica se o botão de configurar foi apertado
-    LDA R0 @SW0TO7
-    STA R0 @HEX1 # exibe valor das chaves
-    JEQ @configDezenas
-    STA R0 @KEY1_RESET
-    STA R0 @LIM_DEZENAS
-    LDI R0 $9
-    CLT R0 @LIM_DEZENAS # verifica se o limite é válido
-    JLT @configDezenas
-    # Se o limite é valido, passa pro próximo
+    STA R0 @KEY1_RESET # limpa botão de configurar valor inicial
+    JSR @reset # reseta contagem
 
     LDI R0 $7 # indica que está setando limite da centena (3 leds acesos)
     STA R0 @LEDR0TO7
@@ -179,12 +166,13 @@ config:
     AND R0 @1
     CEQ R0 @0 # verifica se o botão de configurar foi apertado
     LDA R0 @SW0TO7
+    AND R0 @15
     STA R0 @HEX2 # exibe valor das chaves
     JEQ @configCentenas
     STA R0 @KEY1_RESET
-    STA R0 @LIM_CENTENAS
+    STA R0 @CENTENAS
     LDI R0 $9
-    CLT R0 @LIM_CENTENAS # verifica se o limite é válido
+    CLT R0 @CENTENAS # verifica se o limite é válido
     JLT @configCentenas
     # Se o limite é valido, passa pro próximo
 
@@ -196,12 +184,13 @@ config:
     AND R0 @1
     CEQ R0 @0 # verifica se o botão de configurar foi apertado
     LDA R0 @SW0TO7
+    AND R0 @7
     STA R0 @HEX3 # exibe valor das chaves
     JEQ @configUniMilhares
     STA R0 @KEY1_RESET
-    STA R0 @LIM_UNI_MILHARES
-    LDI R0 $9
-    CLT R0 @LIM_UNI_MILHARES # verifica se o limite é válido
+    STA R0 @UNI_MILHARES
+    LDI R0 $5
+    CLT R0 @UNI_MILHARES # verifica se o limite é válido
     JLT @configUniMilhares
     # Se o limite é valido, passa pro próximo
 
@@ -213,12 +202,13 @@ config:
     AND R0 @1
     CEQ R0 @0 # verifica se o botão de configurar foi apertado
     LDA R0 @SW0TO7
+    AND R0 @7
     STA R0 @HEX4 # exibe valor das chaves
     JEQ @configDezMilhares
     STA R0 @KEY1_RESET
-    STA R0 @LIM_DEZ_MILHARES
-    LDI R0 $9
-    CLT R0 @LIM_DEZ_MILHARES # verifica se o limite é válido
+    STA R0 @DEZ_MILHARES
+    LDI R0 $3
+    CLT R0 @DEZ_MILHARES # verifica se o limite é válido
     JLT @configDezMilhares
     # Se o limite é valido, passa pro próximo
 
@@ -230,33 +220,25 @@ config:
     AND R0 @1
     CEQ R0 @0 # verifica se o botão de configurar foi apertado
     LDA R0 @SW0TO7
+    AND R0 @3
     STA R0 @HEX5 # exibe valor das chaves
     JEQ @configCenMilhares
     STA R0 @KEY1_RESET
-    STA R0 @LIM_CEN_MILHARES
-    LDI R0 $9
-    CLT R0 @LIM_CEN_MILHARES # verifica se o limite é válido
+    STA R0 @CEN_MILHARES
+    LDI R0 $2
+    CLT R0 @CEN_MILHARES # verifica se o limite é válido
     JLT @configCenMilhares
     # Se o limite é valido finaliza configuração
 
     LDI R0 $0 # apaga leds 
     STA R0 @LEDR0TO7
     
-    JSR @restart # reseta valores do display e limpa botões
-    JSR @checkLimit # checa se o limite de contagem foi atingido
+    JSR @restart # resaura configurações iniciais
     RET
 
-# Função para reiniciar contagem
-restart:
+# Função para resetar contagem
+reset:
     LDI R0 $0
-
-    # Limpa botões
-    STA R0 @KEY0_RESET
-    STA R0 @KEY1_RESET
-
-    STA R0 @INC_DISABLE # Limpa flag que inibe incremento
-    STA R0 @LEDR8 # Limpa aviso de overflow
-    STA R0 @LEDR9 # Limpa aviso de limite de contagem
 
     # Limpa valores de contagem
     STA R0 @UNIDADES
@@ -266,6 +248,7 @@ restart:
     STA R0 @DEZ_MILHARES
     STA R0 @CEN_MILHARES
     
+    JSR @restart # reinicia configurações iniciais
     JSR @updateHexas # Atualiza displays
     RET
 
@@ -319,43 +302,9 @@ update:
 
     LDI R0 $1
     STA R0 @INC_DISABLE # Seta flag que inibe incremento
-    STA R0 @LEDR8 # Indica que teve overflow
+    STA R0 @LEDR8 # Indica que chegou no limite de contagem de 24h
 
     endUpdate:
-    RET
-
-# Função de checar limite de contagem
-checkLimit:
-    LDA R0 @UNIDADES 
-    CLT R0 @LIM_UNIDADES # Checa limite da unidade
-    JLT @endCheckLimit # Se não chegou no limite, finaliza função
-
-    LDA R0 @DEZENAS
-    CLT R0 @LIM_DEZENAS # Checa limite da dezena
-    JLT @endCheckLimit # Se não chegou no limite, finaliza função
-    
-    LDA R0 @CENTENAS
-    CLT R0 @LIM_CENTENAS # Checa limite da centena
-    JLT @endCheckLimit # Se não chegou no limite, finaliza função
-
-    LDA R0 @UNI_MILHARES
-    CLT R0 @LIM_UNI_MILHARES # Checa limite da unidade de milhar
-    JLT @endCheckLimit # Se não chegou no limite, finaliza função
-
-    LDA R0 @DEZ_MILHARES
-    CLT R0 @LIM_DEZ_MILHARES # Checa limite da dezena de milhar
-    JLT @endCheckLimit # Se não chegou no limite, finaliza função
-
-    LDA R0 @CEN_MILHARES
-    CLT R0 @LIM_CEN_MILHARES # Checa limite da centena de milhar
-    JLT @endCheckLimit # Se não chegou no limite, finaliza função
-
-    # Se chegou aqui, chegou no limite de contagem
-    LDI R0 $1
-    STA R0 @INC_DISABLE # Seta flag de limite de contagem
-    STA R0 @LEDR9 # indica que o limite de contagem foi atingido
-
-    endCheckLimit:
     RET
 
 # Função para exibir valores da contagem no display
@@ -372,4 +321,22 @@ updateHexas:
     STA R0 @HEX4 # exibe valor da dezena de milhar
     LDA R0 @CEN_MILHARES
     STA R0 @HEX5 # exibe valor da centena de milhar
+    RET
+
+# Função para reiniciar configurações iniciais
+restart:
+    LDI R0 $0
+
+    # Limpa botões
+    STA R0 @KEY0_RESET
+    STA R0 @KEY1_RESET
+    STA R0 @TIMER_RESET # Limpa flag do timer
+
+    # Limpa flags
+    STA R0 @SPEED_STATE # Limpa flag de velocidade
+    STA R0 @SEL_TIMER # Ajusta o timer para 1 segundo
+    STA R0 @INC_DISABLE # Limpa flag que inibe incremento
+    STA R0 @LEDR8 # Limpa aviso de limite de contagem
+    STA R0 @LEDR9 # Limpa aviso de aceleração
+
     RET
