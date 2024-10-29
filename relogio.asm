849dce5e574558d418f1b29919083cbc0421bb4a
beatrizrodriguesf <beatrizrf1@al.insper.edu.br>
2024-10-28 23:54:06 -0300

    Adicionando comentários e trocando nome dos algarismos

--- relogio.asm
+++ relogio.asm
@@ -17,19 +17,19 @@
     #define KEY0_RESET 511
 
 # Setup:
-    #define UNIDADES 16
-    #define DEZENAS 17
-    #define CENTENAS 18
-    #define UNI_MILHARES 19
-    #define DEZ_MILHARES 20
-    #define CEN_MILHARES 21
+    #define UNIDADE_SEGUNDOS 16
+    #define DEZENA_SEGUNDOS 17
+    #define UNIDADE_MINUTOS 18
+    #define DEZENA_MINUTOS 19
+    #define UNIDADE_HORAS 20
+    #define DEZENA_HORAS 21
 
-    #define LIM_UNIDADES 22
-    #define LIM_DEZENAS 23
-    #define LIM_CENTENAS 24
-    #define LIM_UNI_MILHARES 25
-    #define LIM_DEZ_MILHARES 26
-    #define LIM_CEN_MILHARES 27
+    #define LIM_UNIDADE_SEGUNDOS 22
+    #define LIM_DEZENA_SEGUNDOS 23
+    #define LIM_UNIDADE_MINUTOS 24
+    #define LIM_DEZENA_MINUTOS 25
+    #define LIM_UNIDADE_HORAS 26
+    #define LIM_DEZENA_HORAS 27
 
     #define SPEED_STATE 48
 
@@ -49,18 +49,18 @@
         STA R0 @SEL_TIMER
         STA R0 @SPEED_STATE
 
-    # Limpando FlipFlop dos botôes
+    # Limpando FlipFlop dos botões
         STA R0 @KEY0_RESET
         STA R0 @KEY1_RESET
 
     # Inicializando Variáveis
-        LDI R0 $0 # contagem inciada em 0
-        STA R0 @UNIDADES
-        STA R0 @DEZENAS
-        STA R0 @CENTENAS
-        STA R0 @UNI_MILHARES
-        STA R0 @DEZ_MILHARES
-        STA R0 @CEN_MILHARES
+        LDI R0 $0 # contagem iniciada em 0
+        STA R0 @UNIDADE_SEGUNDOS
+        STA R0 @DEZENA_SEGUNDOS
+        STA R0 @UNIDADE_MINUTOS
+        STA R0 @DEZENA_MINUTOS
+        STA R0 @UNIDADE_HORAS
+        STA R0 @DEZENA_HORAS
 
     # Fim do setup
 
@@ -90,7 +90,7 @@ loop:
     JSR @speed
     notSpeed: 
     
-    JSR @update # atualiza valores para ficarem decimais
+    JSR @update # atualiza valores para ficarem decimais e dentro do padrão 23:59:59
     JSR @updateHexas # exibe contagem atualizada
     JMP @loop # fim do loop principal
 
@@ -108,9 +108,9 @@ speed:
 
 # Função para incrementar
 interrupt: # função de incremento chamada por interrupção
-    LDA R1 @UNIDADES
+    LDA R1 @UNIDADE_SEGUNDOS
     ADDI R1 $1 # incrementa
-    STA R1 @UNIDADES
+    STA R1 @UNIDADE_SEGUNDOS
     RETI
 
 # Função para configurar valor inicial
@@ -118,92 +118,93 @@ config:
     STA R2 @KEY1_RESET # limpa botão de configurar valor inicial
     JSR @reset # reseta contagem
 
-    LDI R2 $7 # indica que está setando limite da centena (3 leds acesos)
+    LDI R2 $7 # indica que está setando o início da unidade dos minutos (3 leds acesos)
     STA R2 @LEDR0TO7
 
-    configCentenas:
+    configUnidadeMinutos:
     LDA R2 @KEY1
     ANDI R2 $1
     CEQ R2 @0 # verifica se o botão de configurar foi apertado
     LDA R2 @SW0TO7
     ANDI R2 $15
     STA R2 @HEX2 # exibe valor das chaves
-    JEQ @configCentenas
+    JEQ @configUnidadeMinutos
     STA R2 @KEY1_RESET
-    STA R2 @CENTENAS
+    STA R2 @UNIDADE_MINUTOS
     LDI R2 $9
-    CLT R2 @CENTENAS # verifica se o limite é válido
-    JLT @configCentenas
-    # Se o limite é valido, passa pro próximo
+    CLT R2 @UNIDADE_MINUTOS # verifica se o limite é válido
+    JLT @configUnidadeMinutos
+    # Se o limite é valido, passa para o próximo
 
-    LDI R2 $15 # indica que está setando limite da unidade de milhar (4 leds acesos)
+    LDI R2 $15 # indica que está setando o início da dezena dos minutos (4 leds acesos)
     STA R2 @LEDR0TO7
 
-    configUniMilhares:
+    configDezenaMinutos:
     LDA R2 @KEY1
     ANDI R2 $1
     CEQ R2 @0 # verifica se o botão de configurar foi apertado
     LDA R2 @SW0TO7
     ANDI R2 $7
     STA R2 @HEX3 # exibe valor das chaves
-    JEQ @configUniMilhares
+    JEQ @configDezenaMinutos
     STA R2 @KEY1_RESET
-    STA R2 @UNI_MILHARES
+    STA R2 @DEZENA_MINUTOS
     LDI R2 $5
-    CLT R2 @UNI_MILHARES # verifica se o limite é válido
-    JLT @configUniMilhares
-    # Se o limite é valido, passa pro próximo
+    CLT R2 @DEZENA_MINUTOS # verifica se o limite é válido
+    JLT @configDezenaMinutos
+    # Se o limite é valido, passa para o próximo
 
-    LDI R2 $31 # indica que está setando limite da dezena de milhar (5 leds acesos)
+    LDI R2 $31 # indica que está setando o início da unidade das horas (5 leds acesos)
     STA R2 @LEDR0TO7
 
-    configDezMilhares:
+    configUnidadeHoras:
     LDA R2 @KEY1
     ANDI R2 $1
     CEQ R2 @0 # verifica se o botão de configurar foi apertado
     LDA R2 @SW0TO7
     ANDI R2 $15
     STA R2 @HEX4 # exibe valor das chaves
-    JEQ @configDezMilhares
+    JEQ @configUnidadeHoras
     STA R2 @KEY1_RESET
-    STA R2 @DEZ_MILHARES
+    STA R2 @UNIDADE_HORAS
     LDI R2 $9
-    CLT R2 @DEZ_MILHARES # verifica se o limite é válido
-    JLT @configDezMilhares
-    # Se o limite é valido, passa pro próximo
+    CLT R2 @UNIDADE_HORAS # verifica se o limite é válido
+    JLT @configUnidadeHoras
+    # Se o limite é valido, passa para o próximo
 
-    LDI R2 $63 # indica que está setando limite da centena de milhar (6 leds acesos)
+    LDI R2 $63 # indica que está setando o início da dezena das horas (6 leds acesos)
     STA R2 @LEDR0TO7
 
-    configCenMilhares:
+    configDezenaHoras:
     LDA R2 @KEY1
     ANDI R2 $1
     CEQ R2 @0 # verifica se o botão de configurar foi apertado
     LDA R2 @SW0TO7
     ANDI R2 $3
     STA R2 @HEX5 # exibe valor das chaves
-    JEQ @configCenMilhares
+    JEQ @configDezenaHoras
     STA R2 @KEY1_RESET
-    STA R2 @CEN_MILHARES
-    LDA R2 @DEZ_MILHARES
-    CLTI R2 $4
-    JLT @conf_menor_2
+    STA R2 @DEZENA_HORAS
+    LDA R2 @UNIDADE_HORAS
+    CLTI R2 $4 # Confere se a unidade das horas é menor que 4
+    JLT @ConfigMais20Horas # Se a unidade é menor do que 4, confere se dezena de hora é 0,1 ou 2
     LDI R2 $1
-    CLT R2 @CEN_MILHARES
-    JLT @configCenMilhares
-    conf_menor_2:
+    CLT R2 @DEZENA_HORAS # dezena de hora só poderá ser 0 ou 1
+    JLT @configDezenaHoras
+
+    ConfigMais20Horas: # dezena de hora pode ser 0,1 ou 2
     LDI R2 $2
-    CLT R2 @CEN_MILHARES # verifica se o limite é válido
-    JLT @configCenMilhares
+    CLT R2 @DEZENA_HORAS # verifica se o limite é válido
+    JLT @configDezenaHoras
     # Se o limite é valido finaliza configuração
 
     LDI R2 $0 # apaga leds 
     STA R2 @LEDR0TO7
 
     # Limpa unidade, já que ela continuou incrementando pela interrupção
-    STA R2 @UNIDADES
+    STA R2 @UNIDADE_SEGUNDOS
 
-    JSR @restart # resaura configurações iniciais
+    JSR @restart # restaura configurações iniciais
     RET
 
 # Função para resetar contagem
@@ -211,12 +212,12 @@ reset:
     LDI R2 $0
 
     # Limpa valores de contagem
-    STA R2 @UNIDADES
-    STA R2 @DEZENAS
-    STA R2 @CENTENAS
-    STA R2 @UNI_MILHARES
-    STA R2 @DEZ_MILHARES
-    STA R2 @CEN_MILHARES
+    STA R2 @UNIDADE_SEGUNDOS
+    STA R2 @DEZENA_SEGUNDOS
+    STA R2 @UNIDADE_MINUTOS
+    STA R2 @DEZENA_MINUTOS
+    STA R2 @UNIDADE_HORAS
+    STA R2 @DEZENA_HORAS
     
     JSR @restart # reinicia configurações iniciais
     JSR @updateHexas # Atualiza displays
@@ -224,79 +225,79 @@ reset:
 
 # Função para colocar os valores em decimal
 update:
-    LDA R2 @UNIDADES 
-    CLTI R2 $10 # Verifica se precisa dar "carry out" na unidade
+    LDA R2 @UNIDADE_SEGUNDOS 
+    CLTI R2 $10 # Verifica se precisa dar "carry out" na unidade dos segundos (vai de 0 a 9)
     JLT @endUpdate
     LDI R2 $0 
-    STA R2 @UNIDADES
+    STA R2 @UNIDADE_SEGUNDOS
 
-    LDA R2 @DEZENAS
+    LDA R2 @DEZENA_SEGUNDOS
     ADDI R2 $1
-    STA R2 @DEZENAS
-    CLTI R2 $6 # Verifica se precisa dar "carry out" na dezena
+    STA R2 @DEZENA_SEGUNDOS
+    CLTI R2 $6 # Verifica se precisa dar "carry out" na dezena dos segundos (vai de 0 a 5)
     JLT @endUpdate
     LDI R2 $0
-    STA R2 @DEZENAS
+    STA R2 @DEZENA_SEGUNDOS
 
-    LDA R2 @CENTENAS
+    LDA R2 @UNIDADE_MINUTOS
     ADDI R2 $1
-    STA R2 @CENTENAS
-    CLTI R2 $10 # Verifica se precisa dar "carry out" na centena
+    STA R2 @UNIDADE_MINUTOS
+    CLTI R2 $10 # Verifica se precisa dar "carry out" na unidade dos minutos (vai de 0 a 9)
     JLT @endUpdate
     LDI R2 $0
-    STA R2 @CENTENAS
+    STA R2 @UNIDADE_MINUTOS
 
-    LDA R2 @UNI_MILHARES
+    LDA R2 @DEZENA_MINUTOS
     ADDI R2 $1
-    STA R2 @UNI_MILHARES
-    CLTI R2 $6 # Verifica se precisa dar "carry out" na unidade de milhar
+    STA R2 @DEZENA_MINUTOS
+    CLTI R2 $6 # Verifica se precisa dar "carry out" na dezena dos minutos (vai de 0 a 5)
     JLT @endUpdate
     LDI R2 $0
-    STA R2 @UNI_MILHARES
+    STA R2 @DEZENA_MINUTOS
 
-#   carry_out_hour:
-    LDA R2 @DEZ_MILHARES
+#   Se houver “carry out” para a hora, além de serem chegados os limites dos algarismos, é checado se a hora chegou em 24
+    LDA R2 @UNIDADE_HORAS
     ADDI R2 $1
-    STA R2 @DEZ_MILHARES
-    CEQI R2 $4
-    JEQ @check_24hr
-    JMP @update_cen_milhares
+    STA R2 @UNIDADE_HORAS
+    CEQI R2 $4 # se é 4, verifica o valor da dezena da hora
+    JEQ @check24HR
+    JMP @updadeDezenaHoras # verifica se precisa da “carry out” na unidade de hora
 
-    check_24hr:
-    LDA R2 @CEN_MILHARES
-    CEQI R2 $2
-    JEQ @back_to_0
-    JMP @update_cen_milhares
-    back_to_0:
+    check24HR:
+    LDA R2 @DEZENA_HORAS
+    CEQI R2 $2 # se é 2, é porque a hora chegou em 24 horas
+    JEQ @BackTo0 # reseta contagem se chegou em 24 horas
+    JMP @updadeDezenaHoras # se não chegou em 24 horas, verifica se tem “carry out” na unidade de hora
+    BackTo0:
     JSR @reset
     JMP @endUpdate
 
-    update_cen_milhares:
-    LDA R2 @DEZ_MILHARES
-    CLTI R2 $10
+    updadeDezenaHoras:
+    LDA R2 @UNIDADE_HORAS
+    CLTI R2 $10 # Verifica se tem “carry out” na unidade de hora
     JLT @endUpdate
     LDI R2 $0
-    STA R2 @DEZ_MILHARES
-    LDA R2 @CEN_MILHARES
+    STA R2 @UNIDADE_HORAS
+    LDA R2 @DEZENA_HORAS
     ADDI R2 $1
-    STA R2 @CEN_MILHARES
+    STA R2 @DEZENA_HORAS
     
     endUpdate:
     RET
 
 # Função para exibir valores da contagem no display
 updateHexas:
-    LDA R2 @UNIDADES 
+    LDA R2 @UNIDADE_SEGUNDOS 
     STA R2 @HEX0 # exibe valor da unidade
-    LDA R2 @DEZENAS
+    LDA R2 @DEZENA_SEGUNDOS
     STA R2 @HEX1 # exibe valor da dezena
-    LDA R2 @CENTENAS
+    LDA R2 @UNIDADE_MINUTOS
     STA R2 @HEX2 # exibe valor da centena
-    LDA R2 @UNI_MILHARES
+    LDA R2 @DEZENA_MINUTOS
     STA R2 @HEX3 # exibe valor da unidade de milhar
-    LDA R2 @DEZ_MILHARES
+    LDA R2 @UNIDADE_HORAS
     STA R2 @HEX4 # exibe valor da dezena de milhar
-    LDA R2 @CEN_MILHARES
+    LDA R2 @DEZENA_HORAS
     STA R2 @HEX5 # exibe valor da centena de milhar
     RET
 
